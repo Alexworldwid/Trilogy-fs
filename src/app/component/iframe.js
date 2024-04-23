6106670c7845960758b9c988ee8a62ff6a13caaa
adewale <obadunadewale1234@gmail.com>
2024-04-24 00:32:26 +0100

    dit

--- src/app/component/iframe.js
+++ src/app/component/iframe.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Iframe = () => {
     return (
         <div>
-            <iframe className='w-[450px]  md:w-[600px] lg:w-[500px]' height="415" src="https://www.youtube.com/embed/PiYC5LwfKXo?si=5lwk5jzdsVZ-TVDn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe className='w-[320px]  md:w-[600px] lg:w-[500px]' height="415" src="https://www.youtube.com/embed/PiYC5LwfKXo?si=5lwk5jzdsVZ-TVDn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
         </div>
     );
